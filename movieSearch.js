1072badf806bbafc5dc77025777b07311e471b69
Gage <gagesmith3@gmail.com>
2021-03-25 00:22:37 -0400

    fixed search bar and javascript, removed about me page

---
+++ movieSearch.js
@@ -0,0 +1,20 @@
+
+
+
+const button = document.getElementById('button1');
+button.addEventListener('click', function() {
+    var input = document.getElementById('input').value;
+    
+    var movies = ['Goodfellas', 'Once Upon A Time In Hollywood', 'Spotlight', 'Uncut Gems', 'The Departed', 'Django Unchained', 'Casino Royale', 'Inglorious Bastards', 'The Godfather', 'Disneys "Hercules"'];
+
+
+    var rank = input;
+    rank = rank - 1;
+    
+    movie = movies[rank];
+    if (input > 0 && input < 11) {
+  alert('The rank you selected was ' + input + '. That movie is ' + movie + '.');
+    }
+    else
+    alert('Please select a rank 1-10')
+});
